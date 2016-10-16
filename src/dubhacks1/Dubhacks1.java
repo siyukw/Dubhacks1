98f08b63806a54a217a6a19cc280ca0de8de24db
siyukw <siyukw@gmail.com>
2016-10-16 01:01:48 -0700

    Initial Code Commit
    Initial Commit

---
+++ src/dubhacks1/Dubhacks1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dubhacks1;
+
+/**
+ *
+ * @author siyuk
+ */
+ public class Dubhacks1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/dubhacks1/Number1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dubhacks1;
+
+/**
+ *
+ * @author siyuk
+ */
+public class Number1 {
+    
+}
